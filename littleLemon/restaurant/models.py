89095a4213c2c976e0bbebe9f63a244acfef08da
Micah Bryant <micahbry@umich.edu>
2023-03-17 13:44:25 -0400

    changes made

--- littleLemon/restaurant/models.py
+++ littleLemon/restaurant/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Menu(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+
+    def __str__(self):
+        return self.name
+
+
+class Booking(models.Model):
+    customer_name = models.CharField(max_length=255)
+    phone_number = models.CharField(max_length=15)
+    date = models.DateField()
+    time = models.TimeField()
+    menu = models.ForeignKey(Menu, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.customer_name
